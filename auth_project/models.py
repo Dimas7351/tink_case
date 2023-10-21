28970715d287dab64d29fbe7ca07e51bfb58a314
Дарья Зайцева <m2108448@edu.misis.ru>
2023-10-21 22:02:28 +0300

    neww

---
+++ auth_project/models.py
@@ -0,0 +1,8 @@
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy)
+    username = db.Column(db.String(1000), unique=True)
+    password = db.Column(db.String(1000))
