ec62f5fd228ff729434c0379f7b58ff583b37b9d
darcysoul <catherinesavenko@mail.ru>
2023-10-22 12:32:41 +0300

    all_tournaments with refreshing button

--- src/main/back/app.py
+++ src/main/back/app.py
@@ -18,22 +18,28 @@ class Tournaments(db.Model):
     status = db.Column(db.String(100))
     teams = db.Column(db.String(100))
 
-    def status_class(self):
-        if self.status == 'OPENED':
-            return 'badge-primary'
-        elif self.status == 'ACTIVE':
-            return 'badge-success'
-        elif self.status == 'FINISHED':
-            return 'badge-danger'
-        else:
-            return 'badge-secondary'
-
 @app.route('/')
 def index():
     session = Session()
     tournaments = session.query(Tournaments).all()
     session.close()
     return render_template('all_tournament.html', tournaments=tournaments)
+
+@app.route('/api/refresh', methods=['GET'])
+def refresh_api():
+    session = Session()
+    updated_tournaments = session.query(Tournaments).all()
+    session.close()
+
+    updated_tournaments_list = []
+    for tournament in updated_tournaments:
+        updated_tournaments_list.append({
+            'namem': tournament.namem,
+            'status': tournament.status
+        })
+
+    return jsonify(updated_tournaments_list)
+
 
 
 if __name__ == '__main__':
