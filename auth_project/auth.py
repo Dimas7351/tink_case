5da7161ef8160fe9bdc2e9b8cf41b19088d31e87
Dimas735 <dmamugin@gmail.com>
2023-10-22 18:07:44 +0300

    working /personal

--- auth_project/auth.py
+++ auth_project/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, render_template, request, url_for, flash, jsonify
 from flask_cors import cross_origin
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, verify_jwt_in_request
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
@@ -23,13 +23,12 @@ def login_post():
         print('Invalid credentials')
         return jsonify({'message': 'Invalid credentials'}), 401
 
-    # В случае успешной аутентификации, можно создать access_token.
-    #access_token = create_access_token(identity=username)
-
     print('User logged in successfully')
+
     access_token = create_access_token(identity={'user_id': user.id})
-    return jsonify(access_token=access_token), 201
-    #return jsonify({'message': 'User logged in successfully'}), 201
+    print(user.id, access_token)
+    return jsonify(user_id = user.id, access_token=access_token), 201
+
 
 
 @auth.route('/signup', methods=['POST'])
@@ -52,18 +51,24 @@ def signup_post():
     db.session.commit()
     print('User registered successfully')
     access_token = create_access_token(identity={'user_id': new_user.id})
-    return jsonify(access_token=access_token), 201
+    return jsonify(user_id=new_user.id,access_token=access_token), 201
 
 
 @auth.route('/logout', methods=['POST'])
-
 @cross_origin()
 def logout():
-    # Получаем текущего пользователя
-    #current_user = get_jwt_identity()
+
     print("hahaah")
 
-    # Можно добавить дополнительные операции перед выходом, если нужно
+    return jsonify("message"),200
 
-    #return jsonify(logged_in_as=current_user), 200
-    return jsonify("message"  "sosi"),200
+@auth.route('/takeinfo', methods=['GET'])
+@jwt_required()
+def take_user_info():
+    # Получаем текущего пользователя
+    #current_user = get_jwt_identity()
+    user_id = request.args.get('user_id')
+    user = User.query.filter_by(id=user_id).first()
+
+
+    return jsonify(user_id = user.id,username=user.username), 200
--- auth_project/models.py
+++ auth_project/models.py
@@ -1,3 +1,56 @@
+# from flask_login import UserMixin
+# from . import db
+#
+# #
+# teams_users = db.Table('teams_users',
+#                        db.Column('team_id', db.Integer, db.ForeignKey('teams.id')),
+#                        db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
+#                        )
+#
+# teams_matches = db.Table('teams_matches',
+#                          db.Column('team_id', db.Integer, db.ForeignKey('teams.id')),
+#                          db.Column('match_id', db.Integer, db.ForeignKey('matches.id'))
+#                          )
+#
+#
+# #
+#
+# class Tournament(db.Model):
+#     __tablename__ = 'tournaments'
+#     id = db.Column(db.Integer, primary_key=True)
+#     tourney = db.Column(db.String(100))
+#     status = db.Column(db.String(100))
+#     teams = db.Column(db.String(100))
+#     matches = db.relationship('Match', backref='tournament')
+#
+#
+#
+# class Match(db.Model):
+#     __tablename__ = 'matches'
+#     id = db.Column(db.Integer, primary_key=True)
+#     tournament_id = db.Column(db.Integer(), db.ForeignKey('tournaments.id'))
+#     teams = db.relationship('Team', secondary=teams_matches, backref='matches')
+#     score = db.Column(db.String(100))
+#
+#
+# class Team(db.Model):
+#     __tablename__ = 'teams'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100))
+#
+#
+# #  users = db.relationship('User', secondary=teams_users, backref='teams')
+# #  matches = db.relationship('Match', secondary=teams_matches, backref='teams')
+#
+#
+# class User(UserMixin, db.Model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(1000), unique=True)
+#     password = db.Column(db.String(1000))
+#     teams = db.relationship('Team', secondary=teams_users, backref='users')
+
+
 from . import db
 
 
